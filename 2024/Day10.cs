1056ce55cd18d5f6701f3f4d57d1936d6ecca6ce
JaThePlayer <janmalenta@gmail.com>
2024-12-10 21:23:19 +0100

    Day 10 stupid generics

--- 2024/Day10.cs
+++ 2024/Day10.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
 using CommunityToolkit.HighPerformance;
 
 namespace AoC._2024;
@@ -20,63 +22,117 @@ Part2: memoize
 |------- |---------:|---------:|---------:|----------:|
 | Part1  | 30.68 us | 0.066 us | 0.052 us |      24 B |
 | Part2  | 18.95 us | 0.263 us | 0.246 us |      24 B |
+
+STUPID generics
+| Method | Mean     | Error    | StdDev   | Allocated |
+|------- |---------:|---------:|---------:|----------:|
+| Part1  | 24.51 us | 0.229 us | 0.214 us |      24 B |
+| Part2  | 15.61 us | 0.083 us | 0.073 us |      24 B |
+
+Part2 - use 'byte' for cache
+| Part2  | 15.32 us | 0.022 us | 0.018 us |      24 B |
  */
 public class Day10 : AdventBase
 {
     public override int Year => 2024;
     public override int Day => 10;
 
-    private static int Count(ReadOnlySpan2D<char> span, Span2D<ushort> visited, ushort id, char v, int sx, int sy)
+    private interface IConstBool
+    {
+        public static abstract bool Value { get; }
+    }
+
+    private struct True : IConstBool
+    {
+        public static bool Value => true;
+    }
+
+    private struct False : IConstBool
+    {
+        public static bool Value => false;
+    }
+
+    private interface ITarget
+    {
+        public static abstract char Value { get; }
+    }
+    
+    private struct Target1 : ITarget
+    {
+        public static char Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => '1';
+        }
+    }
+    
+    private struct NextTarget<TInner> : ITarget
+        where TInner : struct, ITarget
+    {
+        public static char Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (char)(TInner.Value + 1);
+        }
+    }
+    
+    private static int Count<TLeft, TRight, TTop, TBot, TTarget>(ReadOnlySpan2D<char> span, Span2D<ushort> visited, ushort id, int sx, int sy)
+        where TLeft : IConstBool
+        where TRight : IConstBool
+        where TTop : IConstBool
+        where TBot : IConstBool
+        where TTarget : struct, ITarget
     {
         ref var visit = ref visited.DangerousGetReferenceAt(sy, sx);
         if (visit == id)
             return 0;
         visit = id;
-        if (v == '9')
+        if (TTarget.Value == '9'+1)
             return 1;
         
-        v += (char)1;
         var r = 0;
         var row = span.GetRowSpan(sy);
 
-        if (sx > 0 && row.DangerousGetReferenceAt(sx-1) == v)
-            r += Count(span, visited, id, v, sx-1, sy);
-        if (row.DangerousGetReferenceAt(sx+1) == v)
-            r += Count(span, visited, id, v, sx+1, sy);
-        if (sy > 0 && span.DangerousGetReferenceAt(sy-1, sx) == v)
-            r += Count(span, visited, id, v, sx, sy-1);
-        if (sy+1 < span.Height && span.DangerousGetReferenceAt(sy+1, sx) == v)
-            r += Count(span, visited, id, v, sx, sy+1);
+        if (TLeft.Value && sx > 0 && row.DangerousGetReferenceAt(sx-1) == TTarget.Value)
+            r += Count<True, False, True, True, NextTarget<TTarget>>(span, visited, id, sx-1, sy);
+        if (TRight.Value && row.DangerousGetReferenceAt(sx+1) == TTarget.Value)
+            r += Count<False, True, True, True, NextTarget<TTarget>>(span, visited, id, sx+1, sy);
+        if (TTop.Value && sy > 0 && span.DangerousGetReferenceAt(sy-1, sx) == TTarget.Value)
+            r += Count<True, True, True, False, NextTarget<TTarget>>(span, visited, id, sx, sy-1);
+        if (TBot.Value && sy+1 < span.Height && span.DangerousGetReferenceAt(sy+1, sx) == TTarget.Value)
+            r += Count<True, True, False, True, NextTarget<TTarget>>(span, visited, id, sx, sy+1);
 
         return r;
     }
     
-    private static int Count2(ReadOnlySpan2D<char> span, Span2D<short> visited, char v, int sx, int sy)
+    private static int Count2<TLeft, TRight, TTop, TBot, TTarget>(ReadOnlySpan2D<char> span, Span2D<byte> visited, int sx, int sy)
+        where TLeft : IConstBool
+        where TRight : IConstBool
+        where TTop : IConstBool
+        where TBot : IConstBool
+        where TTarget : struct, ITarget
     {
+        if (TTarget.Value == '9'+1)
+            return 1;
+        
         ref var cache = ref visited.DangerousGetReferenceAt(sy, sx);
         if (cache > 0)
-            return (short)(cache - 1);
+            return cache - 1;
 
-        if (v == '9')
-        {
-            cache = 2;
-            return 1;
-        }
+        var r = 0;
+        var row = span.GetRowSpan(sy);
+        if (TLeft.Value && sx > 0 && row.DangerousGetReferenceAt(sx-1) == TTarget.Value)
+            r += Count2<True, False, True, True, NextTarget<TTarget>>(span, visited, sx-1, sy);
+        if (TRight.Value && row.DangerousGetReferenceAt(sx+1) == TTarget.Value)
+            r += Count2<False, True, True, True, NextTarget<TTarget>>(span, visited, sx+1, sy);
+        if (TTop.Value && sy > 0 && span.DangerousGetReferenceAt(sy-1, sx) == TTarget.Value)
+            r += Count2<True, True, True, False, NextTarget<TTarget>>(span, visited, sx, sy-1);
+        if (TBot.Value && sy+1 < span.Height && span.DangerousGetReferenceAt(sy+1, sx) == TTarget.Value)
+            r += Count2<True, True, False, True, NextTarget<TTarget>>(span, visited, sx, sy+1);
 
-        v += (char)1;
-        var res = 0;
-        if (sx > 0 && span.DangerousGetReferenceAt(sy, sx-1) == v)
-            res += Count2(span, visited, v, sx-1, sy);
-        if (sy > 0 && span.DangerousGetReferenceAt(sy-1, sx) == v)
-            res += Count2(span, visited, v, sx, sy-1);
-        if (sx+1 < span.Width && span.DangerousGetReferenceAt(sy, sx+1) == v)
-            res += Count2(span, visited, v, sx+1, sy);
-        if (sy+1 < span.Height && span.DangerousGetReferenceAt(sy+1, sx) == v)
-            res += Count2(span, visited, v, sx, sy+1);
+        cache = (byte)(r + 1);
 
-        cache = (short)(res + 1);
-
-        return res;
+        return r;
     }
     
     protected override object Part1Impl()
@@ -99,7 +155,7 @@ public class Day10 : AdventBase
             si += i;
             var x = si % span.Width;
             var y = si / span.Width;
-            sum += Count(span, visited, id++, '0', x, y);
+            sum += Count<True, True, True, True, Target1>(span, visited, id++, x, y);
             
             input = input[(i + 1)..];
             si++;
@@ -115,8 +171,8 @@ public class Day10 : AdventBase
         var lineWidth = input.IndexOf('\n') + 1;
         
         var span = ReadOnlySpan2D<char>.DangerousCreate(input[0], input.Length / lineWidth + 1, lineWidth, 0);
-        Span<short> visited1D = stackalloc short[span.Height * span.Width];
-        var visited = Span2D<short>.DangerousCreate(ref visited1D[0], span.Height, span.Width, 0);
+        Span<byte> visited1D = stackalloc byte[span.Height * span.Width];
+        var visited = Span2D<byte>.DangerousCreate(ref visited1D[0], span.Height, span.Width, 0);
 
         int i;
         var si = 0;
@@ -125,7 +181,7 @@ public class Day10 : AdventBase
             si += i;
             var x = si % span.Width;
             var y = si / span.Width;
-            sum += Count2(span, visited, '0', x, y);
+            sum += Count2<True, True, True, True, Target1>(span, visited, x, y);
             
             input = input[(i + 1)..];
             si++;
