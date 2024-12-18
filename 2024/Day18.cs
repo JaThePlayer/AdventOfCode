3eabe05462e0a5f337b44433be9f34eca1e94156
JaThePlayer <janmalenta@gmail.com>
2024-12-18 13:43:37 +0100

    Day 18 part 2 rework

--- 2024/Day18.cs
+++ 2024/Day18.cs
@@ -2,6 +2,13 @@ using CommunityToolkit.HighPerformance;
 
 namespace AoC._2024;
 
+/*
+After part 2 rework
+| Method | Mean      | Error     | StdDev    | Allocated |
+|------- |----------:|----------:|----------:|----------:|
+| Part1  | 15.564 ms | 0.0541 ms | 0.0506 ms |      30 B |
+| Part2  |  4.604 ms | 0.0233 ms | 0.0218 ms |      39 B |
+ */
 public class Day18 : AdventBase
 {
     private const int MapWidth = 70+1;
@@ -11,7 +18,7 @@ public class Day18 : AdventBase
     public override int Year => 2024;
     public override int Day => 18;
     
-    private bool Visit2<TDir, TDirE>(Span2D<byte> map, Span2D<int> scores, int x, int y, int score)
+    private bool VisitPart1<TDir>(Span2D<byte> map, Span2D<int> scores, int x, int y, int score)
         where TDir : IDirPicker
     {
         ref var storedScore = ref scores.DangerousGetReferenceAt(y, x);
@@ -22,49 +29,65 @@ public class Day18 : AdventBase
         var anySuccess = false;
         
         if (TDir.Right && x + 1 < map.Width && map.DangerousGetReferenceAt(y, x + 1) != '#')
-            anySuccess |= Visit2<ExceptLeftPicker, DirectionRight>(map, scores, x + 1, y, score + 1);
+            anySuccess |= VisitPart1<ExceptLeftPicker>(map, scores, x + 1, y, score + 1);
         if (TDir.Left && x >= 1 && map.DangerousGetReferenceAt(y, x - 1) != '#')
-            anySuccess |= Visit2<ExceptRightPicker, DirectionLeft>(map, scores, x - 1, y, score + 1);
+            anySuccess |= VisitPart1<ExceptRightPicker>(map, scores, x - 1, y, score + 1);
         if (TDir.Up && y >= 1 && map.DangerousGetReferenceAt(y - 1, x) != '#')
-            anySuccess |= Visit2<ExceptDownPicker, DirectionUp>(map, scores, x , y - 1, score + 1);
+            anySuccess |= VisitPart1<ExceptDownPicker>(map, scores, x , y - 1, score + 1);
         if (TDir.Down && y + 1 < map.Height && map.DangerousGetReferenceAt(y + 1, x) != '#')
-            anySuccess |= Visit2<ExceptUpPicker, DirectionDown>(map, scores, x , y + 1, score + 1);
+            anySuccess |= VisitPart1<ExceptUpPicker>(map, scores, x , y + 1, score + 1);
 
         if (y == scores.Height - 1 && x == scores.Width - 1)
             return true;
-        
-        if (!anySuccess)
-        {
-            //map[y, x] = (byte)'#';
-            return false;
-        }
-        
+
         return anySuccess;
     }
     
-    private void VisitBestPathLocations<TDir>(Span2D<byte> map, Span2D<int> scores, 
+    private bool VisitBestPathLocations<TDir>(Span2D<byte> map, Span2D<int> scores, 
         int x, int y, int score, Span2D<byte> visited)
         where TDir : IDirPicker
     {
-        ref var storedScore = ref scores[y, x];
-        if (storedScore != score)
-            return;
+        if (scores[y, x] != score)
+            return false;
         
-        if (map[y, x] == '#' || visited[y, x] > 0)
+        ref var visitedRef = ref visited.DangerousGetReferenceAt(y, x);
+        if (visitedRef > 0)
         {
-            visited[y, x] = 1;
-            return;
+            visitedRef = 1;
+            return false;
         }
-        visited[y, x] = 1;
+        visitedRef = 1;
+
+        if (TDir.Right && x + 1 < map.Width && map[y, x + 1] != '#' && VisitBestPathLocations<ExceptLeftPicker>(map, scores, x + 1, y, score - 1, visited))
+            return true;
+        if (TDir.Left && x >= 1 && map[y, x - 1] != '#' && VisitBestPathLocations<ExceptRightPicker>(map, scores, x - 1, y, score - 1, visited))
+            return true;
+        if (TDir.Up && y >= 1 && map[y - 1, x] != '#' && VisitBestPathLocations<ExceptDownPicker>(map, scores, x , y - 1, score - 1, visited))
+            return true;
+
+        return TDir.Down && y + 1 < map.Height && map[y + 1, x] != '#' && VisitBestPathLocations<ExceptUpPicker>(map, scores, x , y + 1, score - 1, visited);
+    }
+    
+    
+    private bool VisitPart2<TDir>(Span2D<byte> map, Span2D<int> scores, int x, int y, int score)
+        where TDir : IDirPicker
+    {
+        ref var storedScore = ref scores.DangerousGetReferenceAt(y, x);
+        if (storedScore != int.MaxValue)
+            return false;
         
-        if (TDir.Right && x + 1 < map.Width && map[y, x + 1] != '#')
-            VisitBestPathLocations<ExceptLeftPicker>(map, scores, x + 1, y, score - 1, visited);
-        if (TDir.Left && x >= 1 && map[y, x - 1] != '#')
-            VisitBestPathLocations<ExceptRightPicker>(map, scores, x - 1, y, score - 1, visited);
-        if (TDir.Up && y >= 1 && map[y - 1, x] != '#')
-            VisitBestPathLocations<ExceptDownPicker>(map, scores, x , y - 1, score - 1, visited);
-        if (TDir.Down && y + 1 < map.Height && map[y + 1, x] != '#')
-            VisitBestPathLocations<ExceptUpPicker>(map, scores, x , y + 1, score - 1, visited);
+        storedScore = score;
+        
+        if (y == scores.Height - 1 && x == scores.Width - 1)
+            return true;
+
+        if (TDir.Right && x + 1 < map.Width && map.DangerousGetReferenceAt(y, x + 1) != '#' && VisitPart2<ExceptLeftPicker>(map, scores, x + 1, y, score + 1))
+            return true;
+        if (TDir.Up && y >= 1 && map.DangerousGetReferenceAt(y - 1, x) != '#' && VisitPart2<ExceptDownPicker>(map, scores, x , y - 1, score + 1))
+            return true;
+        if (TDir.Down && y + 1 < map.Height && map.DangerousGetReferenceAt(y + 1, x) != '#' && VisitPart2<ExceptUpPicker>(map, scores, x , y + 1, score + 1))
+            return true;
+        return TDir.Left && x >= 1 && map.DangerousGetReferenceAt(y, x - 1) != '#' && VisitPart2<ExceptRightPicker>(map, scores, x - 1, y, score + 1);
     }
     
     protected override object Part1Impl()
@@ -88,7 +111,7 @@ public class Day18 : AdventBase
                 break;
         }
 
-        Visit2<AllDirPicker, DirectionLeft>(map, scores, 0, 0, 0);
+        VisitPart1<AllDirPicker>(map, scores, 0, 0, 0);
 
         return scores[^1, ^1];
     }
@@ -103,9 +126,10 @@ public class Day18 : AdventBase
         var visited = Span2D<byte>.DangerousCreate(ref visited1d[0], MapHeight, MapWidth, 0);
         visited.Fill(0);
         scores1d.Fill(int.MaxValue);
-        Visit2<AllDirPicker, DirectionLeft>(map, scores, 0, 0, 0);
-        //PrintBoard(map, scores);
+        VisitPart2<AllDirPicker>(map, scores, 0, 0, 0);
         VisitBestPathLocations<AllDirPicker>(map, scores, scores.Width - 1, scores.Height - 1, scores[^1, ^1], visited);
+
+        var recalculations = 0;
 
         var i = 0;
         foreach (var (x, y) in Input.TextU8.ParseSplits((byte)'\n', 0, (span, i) =>
@@ -114,23 +138,23 @@ public class Day18 : AdventBase
                      return (x, y);
                  }))
         {
-            //Console.WriteLine(i);
             i++;
             map[y, x] = (byte)'#';
+
+            if (visited[y, x] == 0)
+                continue;
             
-            if (visited[y, x] != 0)
+            // rock just landed on the best path, recalculate!
+            //Console.WriteLine($"RECALCULATING: {i}");
+            recalculations++;
+            visited.Fill(0);
+            scores1d.Fill(int.MaxValue);
+            if (!VisitPart2<AllDirPicker>(map, scores, 0, 0, 0))
             {
-                // rock just landed on the best path, recalculate!
-                //Console.WriteLine($"RECALCULATING: {i}");
-                visited.Fill(0);
-                scores1d.Fill(int.MaxValue);
-                Visit2<AllDirPicker, DirectionLeft>(map, scores, 0, 0, 0);
-                if (scores[^1, ^1] == int.MaxValue)
-                {
-                    return $"{x},{y}"; // 41,26
-                }
-                VisitBestPathLocations<AllDirPicker>(map, scores, scores.Width - 1, scores.Height - 1, scores[^1, ^1], visited);
+                //Console.WriteLine($"Recalc count: {recalculations}"); // 221
+                return $"{x},{y}"; // 41,26
             }
+            VisitBestPathLocations<AllDirPicker>(map, scores, scores.Width - 1, scores.Height - 1, scores[^1, ^1], visited);
         }
 
         return -1;
