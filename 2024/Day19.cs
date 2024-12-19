d81d20f4ab0e59fc8e9100f1720ef9fafe8e1024
JaThePlayer <janmalenta@gmail.com>
2024-12-19 23:40:48 +0100

    D19p2 opts

--- 2024/Day19.cs
+++ 2024/Day19.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using CommunityToolkit.HighPerformance;
@@ -47,6 +48,11 @@ P2 - memoise with length instead of array
 |------- |-----------:|---------:|---------:|-------:|----------:|
 | Part1  |   198.4 us |  2.62 us |  2.45 us | 2.9297 |  25.67 KB |
 | Part2  | 3,339.4 us | 15.75 us | 14.74 us |      - |  26.84 KB |
+
+Using a hashset for towels, and use longs instead of byte[]
+| Part2  | 1.262 ms | 0.0038 ms | 0.0029 ms | 3.9063 |   39.8 KB |
+
+| Part2  | 1.151 ms | 0.0065 ms | 0.0054 ms | 1.9531 |  25.83 KB |
  */
 public class Day19 : AdventBase
 {
@@ -102,63 +108,72 @@ public class Day19 : AdventBase
         }
     }
 
+    private static long ReadBytesAsLong(ReadOnlySpan<byte> span)
+    {
+        long res = 0;
+        for (int i = 0; i < span.Length; i++)
+            res |= (long)span[i] << (i*8);
+        return res;
+    }
+
     protected override object Part2Impl()
     {
         var input = Input.TextU8;
         var lines = input.Split((byte)'\n');
         lines.MoveNext();
         
-        var towels = new List<byte[]>?['z'];
-        foreach (var n in input[lines.Current].ParseSplits((byte)',', 0, (span, _) => span.TrimStart((byte)' ').ToArray()))
+        var towels = new HashSet<long>?['w' + 1];
+        foreach (var n in input[lines.Current].ParseSplits((byte)',', 0, (span, _) => span.TrimStart((byte)' ')))
         {
             ref var col = ref towels[n[^1]];
             col ??= new();
-            col.Add(n);
-        }
-        foreach (var t in towels)
-        {
-            t?.Sort((x1, x2) => x1.Length.CompareTo(x2.Length));
+
+            col.Add(ReadBytesAsLong(n));
         }
 
-        var singleTowelsPossible = new byte['z'];
-        singleTowelsPossible['w'] = towels['w']![0].Length is 1 ? (byte)1 : (byte)0;
-        singleTowelsPossible['u'] = towels['u']![0].Length is 1 ? (byte)1 : (byte)0;
-        singleTowelsPossible['b'] = towels['b']![0].Length is 1 ? (byte)1 : (byte)0;
-        singleTowelsPossible['r'] = towels['r']![0].Length is 1 ? (byte)1 : (byte)0;
-        singleTowelsPossible['g'] = towels['g']![0].Length is 1 ? (byte)1 : (byte)0;
+/*
+        var singleTowelsPossible = new byte['w' + 1];
+        singleTowelsPossible['w'] = towels['w']!.Contains('w') ? (byte)1 : (byte)0;
+        singleTowelsPossible['u'] = towels['u']!.Contains('u') ? (byte)1 : (byte)0;
+        singleTowelsPossible['b'] = towels['b']!.Contains('b') ? (byte)1 : (byte)0;
+        singleTowelsPossible['r'] = towels['r']!.Contains('r') ? (byte)1 : (byte)0;
+        singleTowelsPossible['g'] = towels['g']!.Contains('g') ? (byte)1 : (byte)0;
+        */
 
         lines.MoveNext(); // skip empty line
         
         var memo = new long[64];
         memo[0] = 1;
-        var clearableMemo = memo.AsSpan(2);
+        var clearableMemo = memo.AsSpan(1);
         
         long sum = 0;
         while (lines.MoveNext())
         {
             var line = input[lines.Current];
-            clearableMemo.Fill(long.MaxValue);
+            clearableMemo.Fill(-1);
 
-            memo[1] = singleTowelsPossible.DangerousGetReferenceAt(line[0]);
+            //memo[1] = singleTowelsPossible.DangerousGetReferenceAt(line[0]);
             sum += CheckTowel(line);
         }
-
         return sum; // 606411968721181
         
 
         long CheckTowel(ReadOnlySpan<byte> towel)
         {
             ref var m = ref memo[towel.Length];
-            if (m != long.MaxValue)
+            if (m >= 0)
                 return m;
             
             long sum = 0;
-            foreach (var avail in CollectionsMarshal.AsSpan(towels[towel[^1]]!))
+            var maxLen = int.Min(8, towel.Length);
+            var options = towels[towel[^1]]!;
+            long target = 0;
+            for (int i = 1; i <= maxLen; i++)
             {
-                if (avail.Length > towel.Length)
-                    break;
-                if (towel.EndsWith(avail))
-                    sum += CheckTowel(towel[..^avail.Length]);
+                target |= towel[^i];
+                if (options.Contains(target))
+                    sum += CheckTowel(towel[..^i]);
+                target <<= 8;
             }
 
             m = sum;
