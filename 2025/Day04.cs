73de9cebea3701d254f85cc82f0459d6f00d0c81
JaThePlayer <janmalenta@gmail.com>
2025-12-04 16:16:46 +0100

    D4: TwoDimUtils and P2 rework

--- 2025/Day04.cs
+++ 2025/Day04.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using CommunityToolkit.HighPerformance;
 
 namespace AoC._2025;
@@ -14,43 +15,51 @@ P2 rework: neighCounts
 |------- |---------:|----------:|----------:|-------:|----------:|
 | Part2  | 1.020 ms | 0.0071 ms | 0.0063 ms | 3.9063 |  37.01 KB |
 
+P2: Queue
+| Method | Mean     | Error   | StdDev  | Gen0    | Gen1    | Gen2    | Allocated |
+|------- |---------:|--------:|--------:|--------:|--------:|--------:|----------:|
+| Part2  | 820.2 us | 1.86 us | 1.55 us | 41.0156 | 41.0156 | 41.0156 | 293.33 KB |
+
+Less enqueue
+| Method | Mean     | Error   | StdDev  | Gen0   | Gen1   | Allocated |
+|------- |---------:|--------:|--------:|-------:|-------:|----------:|
+| Part2  | 659.0 us | 2.64 us | 2.34 us | 7.8125 | 0.9766 |  69.25 KB |
+
+Fill(255) to remove map check in EnqueueVisit, map is no longer mutable
+| Method | Mean     | Error   | StdDev  | Gen0   | Gen1   | Allocated |
+|------- |---------:|--------:|--------:|-------:|-------:|----------:|
+| Part2  | 309.6 us | 1.70 us | 1.59 us | 5.8594 | 0.4883 |  50.76 KB |
+
+AggressiveInlining on EnqueueVisit
+| Method | Mean     | Error   | StdDev  | Gen0   | Gen1   | Allocated |
+|------- |---------:|--------:|--------:|-------:|-------:|----------:|
+| Part2  | 268.6 us | 1.98 us | 1.85 us | 5.8594 | 0.4883 |  50.76 KB |
+
+microopt in EnqueueVisit (no more ref, just --)
+| Method | Mean     | Error   | StdDev  | Gen0   | Gen1   | Allocated |
+|------- |---------:|--------:|--------:|-------:|-------:|----------:|
+| Part2  | 259.0 us | 2.79 us | 2.61 us | 5.8594 | 0.4883 |  50.76 KB |
+
+DangerousGetReferenceAt in EnqueueVisit
+| Method | Mean     | Error   | StdDev  | Gen0   | Gen1   | Allocated |
+|------- |---------:|--------:|--------:|-------:|-------:|----------:|
+| Part2  | 251.1 us | 0.64 us | 0.50 us | 5.8594 | 0.4883 |  50.76 KB |
+
+TwoDimUtils
+| Method | Mean      | Error    | StdDev   | Gen0   | Gen1   | Allocated |
+|------- |----------:|---------:|---------:|-------:|-------:|----------:|
+| Part1  |  70.61 us | 1.194 us | 1.226 us |      - |      - |      24 B |
+| Part2  | 211.86 us | 1.097 us | 1.026 us | 6.1035 | 0.4883 |   51976 B |
 */
 public class Day04 : AdventBase
 {
     public override int Year => 2025;
     public override int Day => 4;
 
-    private int CountNeighbors(ReadOnlySpan2D<byte> map, int y, int x)
+    private ref struct NeighborCountVisitor : ITileFilter<byte>
     {
-        var cell = map[y, x];
-        if (cell != '@')
-            return 0;
-
-        var neigh = 0;
-        if (x > 0)
-        {
-            neigh += map[y, x - 1] == '@' ? 1 : 0;
-            if (y > 0)
-                neigh += map[y - 1, x - 1] == '@' ? 1 : 0;
-            if (y < map.Height - 1)
-                neigh += map[y + 1, x - 1] == '@' ? 1 : 0;
-        }
-        
-        if (y > 0)
-            neigh += map[y - 1, x] == '@' ? 1 : 0;
-        if (y < map.Height - 1)
-            neigh += map[y + 1, x] == '@' ? 1 : 0;
-
-        if (x < map.Width - 2)
-        {
-            neigh += map[y, x + 1] == '@' ? 1 : 0;
-            if (y > 0)
-                neigh += map[y - 1, x + 1] == '@' ? 1 : 0;
-            if (y < map.Height - 1)
-                neigh += map[y + 1, x + 1] == '@' ? 1 : 0;
-        }
-
-        return neigh;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Matches(byte tile) => tile == '@';
     }
     
     protected override object Part1Impl()
@@ -66,153 +75,54 @@ public class Day04 : AdventBase
                 if (cell != '@')
                     continue;
 
-                var neigh = 0;
-                if (x > 0)
-                {
-                    neigh += map[y, x - 1] == '@' ? 1 : 0;
-                    if (y > 0)
-                        neigh += map[y - 1, x - 1] == '@' ? 1 : 0;
-                    if (y < map.Height - 1)
-                        neigh += map[y + 1, x - 1] == '@' ? 1 : 0;
-                }
-                
-                if (y > 0)
-                    neigh += map[y - 1, x] == '@' ? 1 : 0;
-                if (y < map.Height - 1)
-                    neigh += map[y + 1, x] == '@' ? 1 : 0;
-
-                if (x < map.Width - 2)
-                {
-                    neigh += map[y, x + 1] == '@' ? 1 : 0;
-                    if (y > 0)
-                        neigh += map[y - 1, x + 1] == '@' ? 1 : 0;
-                    if (y < map.Height - 1)
-                        neigh += map[y + 1, x + 1] == '@' ? 1 : 0;
-                }
-
-                if (neigh < 4)
-                {
+                if (TwoDimUtils.CountNeighborsMatching(ref map, default(NeighborCountVisitor), y, x) < 4)
                     sum++;
-                }
             }
         }
 
         return sum; // 1543
     }
 
+    private readonly ref struct Part2Visitor(Queue<(int y, int x)> toVisit) : ITileVisitor<byte>
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Visit(ref byte count, int y, int x)
+        {
+            if (--count == 3)
+                toVisit.Enqueue((y, x));
+        }
+    }
+    
     protected override object Part2Impl()
     {
-        var map = Input.Create2DMutMap();
+        var map = Input.Create2DMap();
         var neighborCounts = Span2D<byte>.DangerousCreate(ref (new byte[map.Height * map.Width])[0], map.Height, map.Width, 0);
+        neighborCounts.Fill(255);
         var sum = 0;
+        Queue<(int y, int x)> toVisit = new();
         
         for (var y = 0; y < map.Height; y++)
         {
             for (var x = 0; x < map.Width - 1; x++)
             {
-                var neigh = CountNeighbors(map, y, x);
+                var cell = map[y, x];
+                if (cell != '@')
+                    continue;
+                
+                var neigh = TwoDimUtils.CountNeighborsMatching(ref map, default(NeighborCountVisitor), y, x);
                 neighborCounts[y, x] = (byte)neigh;
+                
+                if (neigh < 4)
+                    toVisit.Enqueue((y, x));
             }
         }
 
-        int changes;
-        do
+        var visitor = new Part2Visitor(toVisit);
+        while (toVisit.TryDequeue(out var p))
         {
-            changes = 0;
-            
-            for (var y = 0; y < map.Height; y++)
-            {
-                for (var x = 0; x < map.Width - 1; x++)
-                {
-                    var cell = map[y, x];
-                    if (cell != '@')
-                        continue;
-                    
-                    var neigh = neighborCounts[y, x];
-                    if (neigh < 4)
-                    {
-                        sum++;
-                        changes++;
-                        map[y, x] = (byte)'x';
-                        
-                        if (x > 0)
-                        {
-                            neighborCounts[y, x - 1]--;
-                            if (y > 0)
-                                neighborCounts[y - 1, x - 1]--;
-                            if (y < map.Height - 1)
-                                neighborCounts[y + 1, x - 1]--;
-                        }
-        
-                        if (y > 0)
-                            neighborCounts[y - 1, x]--;
-                        if (y < map.Height - 1)
-                            neighborCounts[y + 1, x]--;
-
-                        if (x < map.Width - 2)
-                        {
-                            neighborCounts[y, x + 1]--;
-                            if (y > 0)
-                                neighborCounts[y - 1, x + 1]--;
-                            if (y < map.Height - 1)
-                                neighborCounts[y + 1, x + 1]--;
-                        }
-                    }
-                }
-            }
-
-        } while (changes > 0);
-        
-        
-        /*
-        var sum = 0;
-        int changes;
-
-        do
-        {
-            changes = 0;
-            for (var y = 0; y < map.Height; y++)
-            {
-                for (var x = 0; x < map.Width - 1; x++)
-                {
-                    var cell = map[y, x];
-                    if (cell != '@')
-                        continue;
-
-                    var neigh = 0;
-                    if (x > 0)
-                    {
-                        neigh += map[y, x - 1] == '@' ? 1 : 0;
-                        if (y > 0)
-                            neigh += map[y - 1, x - 1] == '@' ? 1 : 0;
-                        if (y < map.Height - 1)
-                            neigh += map[y + 1, x - 1] == '@' ? 1 : 0;
-                    }
-
-                    if (y > 0)
-                        neigh += map[y - 1, x] == '@' ? 1 : 0;
-                    if (y < map.Height - 1)
-                        neigh += map[y + 1, x] == '@' ? 1 : 0;
-
-                    if (x < map.Width - 2)
-                    {
-                        neigh += map[y, x + 1] == '@' ? 1 : 0;
-                        if (y > 0)
-                            neigh += map[y - 1, x + 1] == '@' ? 1 : 0;
-                        if (y < map.Height - 1)
-                            neigh += map[y + 1, x + 1] == '@' ? 1 : 0;
-                    }
-
-                    if (neigh < 4)
-                    {
-                        sum++;
-                        map[y, x] = (byte)'x';
-                        changes++;
-                    }
-                }
-            }
-        } while (changes > 0);
-        */
+            sum++;
+            TwoDimUtils.ForEachNeighbor(ref neighborCounts, ref visitor, p.y, p.x);
+        }
 
         return sum; // 9038
     }
---
+++ TwoDimUtils.cs
@@ -0,0 +1,80 @@
+using System.Runtime.CompilerServices;
+using CommunityToolkit.HighPerformance;
+
+namespace AoC;
+
+public interface ITileVisitor<T>
+{
+    public void Visit(ref T value, int y, int x);
+}
+
+public interface ITileFilter<T>
+{
+    public bool Matches(T tile);
+}
+
+public class TwoDimUtils
+{
+    public static void ForEachNeighbor<TChar, TVisitor>(ref Span2D<TChar> map, ref TVisitor visitor, int y, int x)
+        where TVisitor : struct, ITileVisitor<TChar>, allows ref struct
+    {
+        ref var middle = ref Unsafe.AsRef(in map[y, x]);
+        if (y > 0)
+        {
+            ref var el = ref Unsafe.Subtract(ref middle, map.Width);
+            visitor.Visit(ref el, y - 1, x);
+            if (x > 0)
+                visitor.Visit(ref Unsafe.Subtract(ref el, 1), y - 1, x - 1);
+            if (x < map.Width - 2)
+                visitor.Visit(ref Unsafe.Add(ref el, 1), y - 1, x + 1);
+        }
+        
+        if (x > 0)
+            visitor.Visit(ref Unsafe.Subtract(ref middle, 1), y, x - 1);
+        if (x < map.Width - 2)
+            visitor.Visit(ref Unsafe.Add(ref middle, 1), y, x + 1);
+        
+        if (y < map.Height - 1)
+        {
+            ref var el = ref Unsafe.Add(ref middle, map.Width);
+            visitor.Visit(ref el, y + 1, x);
+            if (x > 0)
+                visitor.Visit(ref Unsafe.Subtract(ref el, 1), y + 1, x - 1);
+            if (x < map.Width - 2)
+                visitor.Visit(ref Unsafe.Add(ref el, 1), y + 1, x + 1);
+        }
+    }
+    
+    public static int CountNeighborsMatching<TChar, TFilter>(ref ReadOnlySpan2D<TChar> map, TFilter filter, int y, int x)
+        where TFilter : struct, ITileFilter<TChar>, allows ref struct
+    {
+        var sum = 0;
+        ref var middle = ref Unsafe.AsRef(in map[y, x]);
+        if (y > 0)
+        {
+            ref var el = ref Unsafe.Subtract(ref middle, map.Width);
+            sum += filter.Matches(el) ? 1 : 0;
+            if (x > 0)
+                sum += filter.Matches(Unsafe.Subtract(ref el, 1)) ? 1 : 0;
+            if (x < map.Width - 2)
+                sum += filter.Matches(Unsafe.Add(ref el, 1)) ? 1 : 0;
+        }
+        
+        if (x > 0)
+            sum += filter.Matches(Unsafe.Subtract(ref middle, 1)) ? 1 : 0;
+        if (x < map.Width - 2)
+            sum += filter.Matches(Unsafe.Add(ref middle, 1)) ? 1 : 0;
+        
+        if (y < map.Height - 1)
+        {
+            ref var el = ref Unsafe.Add(ref middle, map.Width);
+            sum += filter.Matches(el) ? 1 : 0;
+            if (x > 0)
+                sum += filter.Matches(Unsafe.Subtract(ref el, 1)) ? 1 : 0;
+            if (x < map.Width - 2)
+                sum += filter.Matches(Unsafe.Add(ref el, 1)) ? 1 : 0;
+        }
+
+        return sum;
+    }
+}
